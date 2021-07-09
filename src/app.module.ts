c18a7befd8ef2f93550d3ddeb4f9f615e95d24bc
AlexR0v <mail.ponomarev@yandex.ru>
2021-07-09 12:41:11 +0300

    feat: add migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { TagModule } from './tag/tag.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import ormconfig from './ormconfig'
 
 @Module({
-  imports: [ TypeOrmModule.forRoot(), TagModule ]
+  imports: [ TypeOrmModule.forRoot(ormconfig), TagModule ]
 })
 export class AppModule {
 }
