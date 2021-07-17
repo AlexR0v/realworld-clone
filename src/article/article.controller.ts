97a84945de5cfbec262fbb37c6ad2197cc48e4a1
AlexR0v <mail.ponomarev@yandex.ru>
2021-07-17 14:17:32 +0300

    feat: add articles search

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,18 +1,40 @@
-import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
 import { ArticleService } from './article.service'
 import { AuthGuard } from '../user/guards/auth.guard'
 import { UserDecorator } from '../user/decorators/user.decorator'
 import { UserEntity } from '../user/user.entity'
 import { ArticleResponseInterface } from './types/articleResponse.interface'
 import { CreateArticleDto } from './dto/create-article.dto'
+import { ArticlesResponseInterface } from './types/articlesResponse.interface'
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {
   }
 
+  @Get()
+  async getAll(
+    @UserDecorator('id') currentUserId: number,
+    @Query() query: any
+  ): Promise<ArticlesResponseInterface> {
+    return this.articleService.findAll(currentUserId, query)
+  }
+
   @Post()
   @UseGuards(AuthGuard)
+  @UsePipes(new ValidationPipe())
   async create(
     @UserDecorator() currentUser: UserEntity,
     @Body('article') createArticleDto: CreateArticleDto
@@ -33,5 +55,16 @@ export class ArticleController {
   @UseGuards(AuthGuard)
   async deleteArticle(@UserDecorator('id') currentUserId: number, @Param('slug') slug: string) {
     return await this.articleService.deleteArticle(slug, currentUserId)
+  }
+
+  @Put(':slug')
+  @UseGuards(AuthGuard)
+  async updateArticle(
+    @UserDecorator('id') currentUserId: number,
+    @Param('slug') slug: string,
+    @Body('article') updateArticleDto: CreateArticleDto
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.updateArticle(slug, currentUserId, updateArticleDto)
+    return this.articleService.buildArticleResponse(article)
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,15 +1,55 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { ArticleEntity } from './article.entity'
 import { InjectRepository } from '@nestjs/typeorm'
-import { DeleteResult, Repository } from 'typeorm'
+import { DeleteResult, getRepository, Repository } from 'typeorm'
 import { UserEntity } from '../user/user.entity'
 import { CreateArticleDto } from './dto/create-article.dto'
 import { ArticleResponseInterface } from './types/articleResponse.interface'
 import slugify from 'slugify'
+import { ArticlesResponseInterface } from './types/articlesResponse.interface'
 
 @Injectable()
 export class ArticleService {
-  constructor(@InjectRepository(ArticleEntity) private readonly articleRepo: Repository<ArticleEntity>) {
+  constructor(
+    @InjectRepository(ArticleEntity) private readonly articleRepo: Repository<ArticleEntity>,
+    @InjectRepository(UserEntity) private readonly userRepo: Repository<UserEntity>
+  ) {
+  }
+
+  async findAll(userId: number, query: any): Promise<ArticlesResponseInterface> {
+    const queryBuilder = getRepository(ArticleEntity)
+      .createQueryBuilder('articles')
+      .leftJoinAndSelect('articles.author', 'author')
+
+    queryBuilder.orderBy('articles.createdAt', 'DESC')
+    const articlesCount = await queryBuilder.getCount()
+
+    if (query.tag) {
+      queryBuilder.andWhere('articles.tagList LIKE :tag', {
+        tag: `%${query.tag}%`
+      })
+    }
+
+    if (query.author) {
+      const author = await this.userRepo.findOne({
+        username: query.author
+      })
+      queryBuilder.andWhere('articles.author = :id', {
+        id: author.id
+      })
+    }
+
+    if (query.limit) {
+      queryBuilder.limit(query.limit)
+    }
+
+    if (query.offset) {
+      queryBuilder.offset(query.offset)
+    }
+
+    const articles = await queryBuilder.getMany()
+
+    return { articles, articlesCount }
   }
 
   async createArticle(user: UserEntity, createArticle: CreateArticleDto): Promise<ArticleEntity> {
@@ -23,6 +63,16 @@ export class ArticleService {
     article.slug = ArticleService.getSlug(createArticle.title)
 
     article.author = user
+    return await this.articleRepo.save(article)
+  }
+
+  async updateArticle(slug: string, userId: number, updateArticle: CreateArticleDto): Promise<ArticleEntity> {
+    const article = await this.getArticle(slug)
+    if (article.author.id !== userId) {
+      throw  new HttpException('You are not author', HttpStatus.FORBIDDEN)
+    }
+    console.log(updateArticle)
+    Object.assign(article, updateArticle)
     return await this.articleRepo.save(article)
   }
 
